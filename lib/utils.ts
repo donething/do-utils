018dc634f2fdd8f6ae1fdf5514a2e96bdc5a63c1
Donething <donething.net@gmail.com>
2021-11-25 10:08:21 +0800

    添加函数：waitForElem insertJS insertJSSrc elemOf showMsg showMsgInBG

--- lib/utils.ts
+++ lib/utils.ts
@@ -65,6 +65,17 @@ export const sleep = async function (time: number) {
 }
 
 /**
+ * 等待直到指定的元素存在（需在async函数中await调用）
+ * @param selector CSS元素选择器
+ * @param interval 等待判断的间隔时间（单位毫秒，不指定时为300毫秒）
+ */
+export const waitForElem = async function (selector: string, interval = 300) {
+  while (!document.querySelector(selector)) {
+    await sleep(interval)
+  }
+}
+
+/**
  * 返回可读的日期
  * @param date 日期
  * @param askHour12 是否以`12时制`返回，默认 false
@@ -281,4 +292,125 @@ export const copyText = function (doc: Document, text: string) {
   textarea.select()
   doc.execCommand("copy")
   textarea.remove()
+}
+
+/**
+ * 插入内联 JavaScript
+ * @param code JavaScript 代码
+ * @return 脚本所在的HTML元素
+ */
+export const insertJS = function (code: string | Function): HTMLScriptElement {
+  let script = document.createElement("script")
+  // 使用匿名函数是为了避免干扰原执行环境，如报变量已存在的错误
+  script.textContent = '(' + code + ')()';
+  // 添加到网页中
+  (document.head || document.documentElement).appendChild(script)
+
+  return script
+}
+
+/**
+ * 插入内联 JavaScript
+ * @param src 注入脚步的路径
+ * @return 脚本所在的HTML元素
+ */
+export const insertJSSrc = function (src: string): HTMLScriptElement {
+  let script = document.createElement("script")
+  // 注入脚本，不能使用textContent属性，会报错 inline code
+  script.src = src;
+  // 添加到网页中
+  (document.head || document.documentElement).appendChild(script)
+
+  return script
+}
+
+/**
+ * 将 HTML 字符串转换为 HTMLElement 对象
+ *
+ * 1. 如果只有一个顶层元素，那么返回该元素
+ *
+ * 2. 如果该有多个顶层元素，则用 div 包裹后返回
+ *
+ * 此方法会自动下载其中的图片等资源，如果仅需解析为 DOM，可以用 domParser
+ *
+ * @param str 需解析的字符串
+ * @return Element 对象
+ * @see https://stackoverflow.com/a/494348/8179418
+ */
+export const elemOf = function (str: string): Element {
+  let div = document.createElement('div')
+  div.innerHTML = str
+
+  // Change this to div.childNodes to support multiple top-level nodes
+  if (div.children.length === 1) {
+    return div.firstElementChild as Element
+  }
+  return div
+}
+
+// 消息的类型
+export const Msg = {info: "info", success: "success", warning: "warning", error: "error"}
+/**
+ * 在网页内显示消息提示（仅在内容脚本中可用）
+ * @param message 消息都内容
+ * @param type 消息都类型，可选 MSG 中的值。默认为"info"
+ * @param duration 消息都持续时间（毫秒）。默认 3 秒
+ */
+export const showMsg = async function (message: string, type = Msg.info, duration = 3000) {
+  // 获取消息的的弹窗，以及消息项。消息弹窗内可以放置多条消息项
+  let htmlURL = chrome.runtime.getURL("/htmls/message.html")
+  let resp = await fetch(htmlURL)
+  let html = elemOf(await resp.text())
+
+  // 消息盒子，内含消息。已存在则重用，不存在则用新的
+  let msgBox = document.querySelector("#do-msgbox") || html.querySelector("#do-msgbox")
+  // 消息项
+  let msg = html.querySelector(".do-msg")
+
+  // 根据消息类型，显示内容
+  // 显示消息的图标
+  // @ts-ignore
+  msg.querySelector(`.do-msg-${type}`).style.display = "inline"
+  // 显示消息的文本
+  // @ts-ignore
+  msg.querySelector(".do-msg-text").innerText = message
+  // 弹出消息
+  // @ts-ignore
+  document.body.appendChild(msgBox)
+
+  // 添加到消息盒子中（添加动画）
+  // @ts-ignore
+  msgBox.appendChild(msg)
+
+  // 到时即移除消息项
+  setTimeout(() => {
+    // 移除消息项
+    // @ts-ignore
+    msg.style.display = "none"
+  }, duration)
+}
+
+/**
+ * 在网页内显示消息提示（仅在后台脚本中可用）
+ * @param message 消息都内容
+ * @param type 消息都类型，可选 MSG 中的值。默认为"info"
+ * @param duration 消息都持续时间（毫秒）。默认 3 秒
+ */
+const showMsgInBG = (message: string, type = Msg.info, duration = 3000) => {
+  chrome.tabs.query({active: true}, tabs => {
+    if (tabs.length === 0 || !tabs[0].id) {
+      console.log("后台复制文本出错，tabs 信息不完整", tabs)
+      throw "后台复制文本出错，tabs 信息不完整"
+    }
+
+    // 向当前标签中注入文本框，以复制指定文本
+    chrome.scripting.executeScript({
+      target: {tabId: tabs[0].id},
+      func: (m: string, t: string, d: number) => {
+        // 发送复制操作的消息
+        showMsg(m, t, d)
+      },
+      args: [message, type, duration]
+    })
+  })
 }
